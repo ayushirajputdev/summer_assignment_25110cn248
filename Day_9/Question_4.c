c06cf738ccaa52bcd5faea567eefc34e152e87f9
AYUSHI <ayushirajput2405@gmail.com>
2026-06-05 23:23:34 +0530

    day 9 assignment completed

---
+++ Day_9/Question_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=5;i++){
+        printf("\n");
+        for(j=1;j<=5;j++){
+            if(i==1||i==5){
+                printf("*");
+            }
+            else {
+               if(j==1||j==5){
+                printf("*");
+               }
+               else{
+                printf(" ");
+               }
+            }
+        }
+    }
+    
+    return 0;
+}
