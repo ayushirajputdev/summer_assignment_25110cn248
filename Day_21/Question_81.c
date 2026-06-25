c4154761b7d39b5f590c4b917d1f802bda45d382
AYUSHI <ayushirajput2405@gmail.com>
2026-06-25 09:33:33 +0530

    assesment include about array

---
+++ Day_21/Question_81.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+   char str[1000];
+    int length = 0;
+
+   
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    
+
+    
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("Length of the string is: %d\n", length);
+    return 0;
+}
